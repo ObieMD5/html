496b3ab5002c6bce76b65582c5b1fc71c88aff35
Jonmichael Chambers <jon@tyrbo.net>
2016-07-25 18:52:43 +0000

    Just fooling around with React

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
 import App from './App';
 import './index.css';
 
+import 'normalize.css/normalize.css'
+
 ReactDOM.render(
-  <App />,
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+    </Route>
+  </Router>,
   document.getElementById('root')
 );
